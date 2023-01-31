267c326914569b6b0304d2d9967d64a473aa25f3
Sane Generous <karim2k11@gmail.com>
2023-01-31 15:09:20 +0000

    Initial Project Add

---
+++ src/main/java/org/tp/haustierklinik/HaustierKlinikApplication.java
@@ -0,0 +1,13 @@
+package org.tp.haustierklinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaustierKlinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaustierKlinikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tp/haustierklinik/HaustierKlinikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tp.haustierklinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaustierKlinikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
