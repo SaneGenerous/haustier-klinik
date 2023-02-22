bb053e410764950f3faf8a5db32ba18746396bf2
Sane Generous <karim2k11@gmail.com>
2023-02-22 14:55:09 +0000

    Refactoring Map Impl to abstract ID generation, closes #18

--- src/main/java/org/tp/haustierklinik/services/map/AbstractMapService.java
+++ src/main/java/org/tp/haustierklinik/services/map/AbstractMapService.java
@@ -1,20 +1,27 @@
 package org.tp.haustierklinik.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.tp.haustierklinik.model.BaseEntity;
 
-public class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
     T findById(ID id){
         return map.get(id);
     }
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+        if(object != null) {
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("The object cannot be null");
+        }
+
         return object;
     }
     void deleteById(ID id){
@@ -23,4 +30,14 @@ public class AbstractMapService<T, ID> {
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
+        private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId =Collections.max(map.keySet()) + 1;
+        }
+        catch(NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
+        }
 }
