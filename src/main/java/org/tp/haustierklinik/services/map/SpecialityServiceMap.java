eb56e4c00fc084e59a4046b7b8236d9db88b2ddf
Sane Generous <karim2k11@gmail.com>
2023-03-01 15:20:54 +0000

    Added Specialities Map Impl closes #26

---
+++ src/main/java/org/tp/haustierklinik/services/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package org.tp.haustierklinik.services.map;
+
+import org.tp.haustierklinik.model.Speciality;
+import org.tp.haustierklinik.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        super.findById(id);
+        return null;
+    }
+}
