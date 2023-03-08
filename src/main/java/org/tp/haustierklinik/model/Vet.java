ac74f47bda29a2ccb1b16020c5430793fa650261
Sane Generous <karim2k11@gmail.com>
2023-03-08 16:02:46 +0000

    update Vets JPA entity mapping closes #31

--- src/main/java/org/tp/haustierklinik/model/Vet.java
+++ src/main/java/org/tp/haustierklinik/model/Vet.java
@@ -1,11 +1,16 @@
 package org.tp.haustierklinik.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
     private Set<Speciality> specialities = new HashSet<>();
-
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialies", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
